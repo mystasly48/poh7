7240379a8fa939d1b1b6684fb7916e3fb4c62e4c
Mystasly <mystasly48@gmail.com>
2016-09-08 19:55:58 +0900

    Updated ニーソ
    完了

--- C#/KneeSocks.cs
+++ C#/KneeSocks.cs
@@ -1,16 +1,28 @@
 using System;
 public class Hello {
   public static void Main() {
-    var count = int.Parse(Console.ReadLine());
-    var length = int.Parse(Console.ReadLine());
+    var count = int.Parse(Console.ReadLine()); // 3
+    var length = int.Parse(Console.ReadLine()); // 10
     var result = "";
-    while (true) {
-      if (result.Length != ) {
-
+    var appendChar = "R";
+    while(true) {
+      for (int i = 0; i < count; i++) {
+        if (result.Length < length) {
+          result += appendChar;
+        } else {
+          break;
+        }
+      }
+      if (result.Length == length) {
+        break;
       } else {
-
+        if (appendChar == "R") {
+          appendChar = "W";
+        } else {
+          appendChar = "R";
+        }
       }
     }
-    }
+    Console.WriteLine(result);
   }
 }
