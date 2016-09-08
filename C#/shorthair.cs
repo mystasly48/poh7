dc4cb251fc473c8bb6d306838f1b0fc0ea5d42f2
Mystasly <mystasly48@gmail.com>
2016-09-08 20:26:57 +0900

    Added ショートヘア

---
+++ C#/shorthair.cs
@@ -0,0 +1,8 @@
+using System;
+public class Hello {
+  public static void Main() {
+    var a = int.Parse(Console.ReadLine());
+    var b = int.Parse(Console.ReadLine());
+    Console.WriteLine(a + b);
+  }
+}
