8c633d333a5245d5ea079bd2a3b90d461001837a
Maisu <mystasly48@gmail.com>
2016-09-08 17:11:49 +0900

    Added ニーソ
    まだ未完成

---
+++ C#/KneeSocks.cs
@@ -0,0 +1,16 @@
+using System;
+public class Hello {
+  public static void Main() {
+    var count = int.Parse(Console.ReadLine());
+    var length = int.Parse(Console.ReadLine());
+    var result = "";
+    while (true) {
+      if (result.Length != ) {
+
+      } else {
+
+      }
+    }
+    }
+  }
+}
