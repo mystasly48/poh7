c5250696f4d29ad914450b7e5cbd5bff2868338e
Mystasly <mystasly48@gmail.com>
2016-09-08 20:26:48 +0900

    Added つり目

---
+++ C#/tsurime.cs
@@ -0,0 +1,9 @@
+using System;
+public class Hello {
+  public static void Main() {
+    var count = int.Parse(Console.ReadLine());
+    for (int i = 0; i < count; i++) {
+        Console.Write("Ann");
+    }
+  }
+}
