2a45067754062ccc96c6f7237c61653072e96267
Maisu <mystasly48@gmail.com>
2016-09-09 16:09:15 +0900

    Added メイド服
    まだ作業中

---
+++ C#/maid.cs
@@ -0,0 +1,16 @@
+using System;
+public class Hello {
+  public static void Main() {
+    var sleepTime = 2500;
+    var count = int.Parse(Console.ReadLine());
+    var overTimes = new int[count];
+    var appendTimes = new int[count];
+    var sleepTimes = new int[count];
+    for (int i = 0; i < count; i++) overtimes[i] = int.Parse(Console.ReadLine());
+    for (int i = 0; i < count; i++) appendTimes[i] = overtimes[i] / 3;
+    for (int i = 0; i < count; i++) sleepTimes[i] = sleepTime - appendTimes[i];
+    for (int i = 0; i < count; i++) {
+      if ()
+    }
+  }
+}
